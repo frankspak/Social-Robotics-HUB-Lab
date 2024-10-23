c6dc4889f43f72fd6ec9de2f8f7599e097972227
StanHoeven <stanhoeven111@outlook.com>
2024-10-23 12:54:48 +0200

    forgot to save tinyllama bat file

--- run_tinyllama.bat
+++ run_tinyllama.bat
@@ -1,4 +1,6 @@
 @echo off
+start cmd /k uvicorn display:app --host 0.0.0.0 --port 8000
+timeout /t 10
 start cmd /k python3 startDialogueServer.py --server tinyllama
 timeout /t 10
 start cmd /k python module_speechrecognition.py --pip 192.168.1.140
